320245f758456d8277bf10e8d956b208998743df
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-04-12 17:27:02 -0300

    add AddNoteButton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,16 @@
 import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView } from '../view'
-//import { NothingSelectedView } from '../view/NothingSelectedView'
+//import { NoteView } from '../view'
+import { NothingSelectedView } from '../view/NothingSelectedView'
+
+import { AddNoteButton } from '../components/AddNoteButton'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
+      {/*<NoteView />*/}
+      <NothingSelectedView />
+
+      <AddNoteButton />
     </JournalLayout>
   )
 }
