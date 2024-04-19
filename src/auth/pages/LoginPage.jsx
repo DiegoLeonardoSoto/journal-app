1a39d5b1cec918e4eabb2a7558c1acce222451e7
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-04-19 15:20:58 -0300

    add auth functionality

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -4,17 +4,23 @@ import { Link } from 'react-router-dom'
 import { AuthLayout } from '../layout/AuthLayout'
 import { useForm } from '../../hooks'
 import { Google } from '@mui/icons-material'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   checkingAuthentication,
   startGoogleSignIn
 } from '../../store/auth/thunks'
+import { STATUS } from '../../store/auth/authSlice'
+import { useMemo } from 'react'
 
 export const LoginPage = () => {
+  const { status } = useSelector((state) => state.auth)
+
   const { email, password, onInputChange } = useForm({
     email: 'diego@google.com',
     password: '123456'
   })
+
+  const isAuthenticating = useMemo(() => status === STATUS.CHECKING, [status])
 
   const dispatch = useDispatch()
 
@@ -59,13 +65,23 @@ export const LoginPage = () => {
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth type="submit">
+              <Button
+                disabled={isAuthenticating}
+                variant="contained"
+                fullWidth
+                type="submit"
+              >
                 Login
               </Button>
             </Grid>
 
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth onClick={onGoogleSignIn}>
+              <Button
+                disabled={isAuthenticating}
+                variant="contained"
+                fullWidth
+                onClick={onGoogleSignIn}
+              >
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,9 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export const STATUS = {
+  CHECKING: 'checking',
+  NOT_AUTH: 'not-authenticated',
+  AUTH: 'authenticated'
+}
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    status: 'not-authenticated', //checking, not-authenticated, 'authenticated'
+    status: STATUS.NOT_AUTH, //checking, not-authenticated, 'authenticated'
     uid: null,
     email: null,
     displayName: null,
@@ -11,10 +17,24 @@ export const authSlice = createSlice({
     errorMessage: null
   },
   reducers: {
-    login: (state, action) => {},
-    logout: (state, payload) => {},
+    login: (state, { payload }) => {
+      state.status = STATUS.AUTH //checking, not-authenticated, 'authenticated'
+      state.uid = payload.uid
+      state.email = payload.email
+      state.displayName = payload.displayName
+      state.photoURL = payload.photoURL
+      state.errorMessage = null
+    },
+    logout: (state, { payload }) => {
+      state.status = STATUS.NOT_AUTH //checking, not-authenticated, 'authenticated'
+      state.uid = null
+      state.email = null
+      state.displayName = null
+      state.photoURL = null
+      state.errorMessage = payload.errorMessage
+    },
     checkingCredentials: (state) => {
-      state.status = 'checking'
+      state.status = STATUS.CHECKING
     }
   }
 })
