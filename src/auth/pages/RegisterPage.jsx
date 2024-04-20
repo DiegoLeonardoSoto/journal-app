dfdaa8795ef95594649ec9326b1495509139e430
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-04-20 15:25:28 -0300

    add animations

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -55,7 +55,10 @@ export const RegisterPage = () => {
   return (
     <AuthLayout title="Crear cuenta">
       <h1>FormValid: {isFormValid ? 'true' : 'false'}</h1>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        className="animate__animated animate__fadeIn animate__faster"
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
